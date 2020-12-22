6c37f917a5d936c00acfd8defaf7ce3d2f71fb0b
elid venega <venegaelid@gmail.com>
2020-12-21 20:01:46 -0800

    js udemy mastery

---
+++ 01-Fundamentals-Part-1/starter/js/assignment.js
@@ -0,0 +1,6 @@
+let country = "U.S.A";
+let continent = "North America";
+let population = "328.2 million";
+console.log(country);
+console.log(continent);
+console.log(population);
---
+++ 01-Fundamentals-Part-1/starter/js/main.js
@@ -0,0 +1,16 @@
+// let js = 'amazing';
+// if (js === 'amazing') alert('Javascript');
+
+// value
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Jonas";
+console.log(firstName);
+
+// descriptive variable names
+
+let myFirstJob = "Caregiver";
+let myCurrentJob = "Lab Tech";
+
+console.log(myFirstJob);
