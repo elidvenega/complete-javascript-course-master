0d165c1ad4fd74260781790a265e4bd8176e3ff1
elid venega <venegaelid@gmail.com>
2020-12-23 16:09:50 -0800

    udemy course

--- 01-Fundamentals-Part-1/starter/js/assignment.js
+++ 01-Fundamentals-Part-1/starter/js/assignment.js
@@ -1,6 +1,44 @@
-let country = "U.S.A";
-let continent = "North America";
+const country = "U.S.A";
+const continent = "North America";
 let population = "328.2 million";
 console.log(country);
 console.log(continent);
 console.log(population);
+
+// Exercise data types
+const isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof country);
+console.log(typeof population);
+console.log(typeof language);
+
+//  const and let exercise
+const country = "U.S.A";
+const continent = "North America";
+let language = "english";
+
+country / 2
+population + 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- 01-Fundamentals-Part-1/starter/js/main.js
+++ 01-Fundamentals-Part-1/starter/js/main.js
@@ -1,16 +1,105 @@
-// let js = 'amazing';
-// if (js === 'amazing') alert('Javascript');
+// // let js = 'amazing';
+// // if (js === 'amazing') alert('Javascript');
 
-// value
-console.log("Jonas");
-console.log(23);
+// // value
+// console.log("Jonas");
+// console.log(23);
 
-let firstName = "Jonas";
-console.log(firstName);
+// let firstName = "Jonas";
+// console.log(firstName);
 
-// descriptive variable names
+// // descriptive variable names
 
-let myFirstJob = "Caregiver";
-let myCurrentJob = "Lab Tech";
+// let myFirstJob = "Caregiver";
+// let myCurrentJob = "Lab Tech";
 
-console.log(myFirstJob);
+// console.log(myFirstJob);
+
+/*let a = "helloooo";
+
+console.log(typeof a);
+
+// Dynamic typing
+let javascriptIsFun = true;
+console.log(typeof javascriptIsFun);
+
+javascriptIsFun = 'YES';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(year);
+
+// a bug thats not corrected
+console.log(typeof null); */
+
+/*let age = 30;
+age= 31;
+console.log(age);
+
+const birthYear = 1991;
+//birthYear = 1990;
+
+var job = 'programmer';
+job = 'teacher';
+console.log(job);*/
+
+// operators
+
+/*const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 1998;
+console.log(ageSarah,ageJonas);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+
+const firstName = "Jonas";
+const lastName = "Schmedtmann";
+
+console.log(firstName + ' ' + lastName);
+
+let x = 10 + 5;*/
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2023;
+
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas,ageSarah, averageAge);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
