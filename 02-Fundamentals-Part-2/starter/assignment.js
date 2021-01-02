589af905d968404dff38e8dacd1b1d3cac41a29e
elid venega <venegaelid@gmail.com>
2021-01-02 11:41:58 -0800

    work on second part on udemy challenges

--- 02-Fundamentals-Part-2/starter/assignment.js
+++ 02-Fundamentals-Part-2/starter/assignment.js
@@ -10,3 +10,34 @@ const country1 = describeCountry("Konoha", "10,000","Village in leaf");
 console.log(country1);
 const country2 = describeCountry("US","330 milion", "Washington");
 console.log(country2);
+
+function percentageOfWorld1(population) {
+const percent = population / 7900 * 100;
+return percent;
+}
+
+console.log(percentageOfWorld1(1441));
+
+
+const percentageOfWorld2 = function(population) {
+const percent = population / 7900 * 100;
+return percent;
+
+}
+
+console.log(percentageOfWorld2(900));
+
+
+const percentageOfWorld3 = (population) => {
+const percent = population / 7900 * 100;
+return percent;
+};
+console.log(percentageOfWorld3(1200));
+
+
+
+
+
+
+
+
