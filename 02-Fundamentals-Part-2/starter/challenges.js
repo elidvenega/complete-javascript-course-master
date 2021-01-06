79e39075af507f56a55d24e2f1513bf2cd07d67b
elid venega <venegaelid@gmail.com>
2021-01-05 20:19:39 -0800

    work on udemy js function challenge

--- 02-Fundamentals-Part-2/starter/challenges.js
+++ 02-Fundamentals-Part-2/starter/challenges.js
@@ -1,41 +1,47 @@
-const calcAverage = (score1, score2, score3) => {
-    return (score1 + score2 + score3) / 3;
-}
+const calcAverage =  (a, b, c) => (a + b + c) / 3;
 
-const checkWinner = function (dolphins, koalas) {
-        const avgScore = calcAverage(score1, score2, score3);
+const scoreDolphins = calcAverage(44,23,71);
+const scoreKoalas = calcAverage(65,54,40);
 
-        if (dolphins <= avgScore) {
-            console.log('dolphin wins');
-        } else if (koalas <= avgScore) {
-            console.log('koalas wins');
-        }
 
+const checkWinner = function (avgDolphins, avgKoalas) {
+    
+    if (avgDolphins  >= 2 * avgKoalas) {
+        console.log('dolphin wins');
+        
+    } else if (avgKoalas >=  2 * avgDolphins) {
+        console.log('koalas wins');
+
+    } else {
+        console.log('No team wins...');
     }
 
-    console.log(checkWinner(22,30));
+}
+
+checkWinner(scoreDolphins,scoreKoalas);
+checkWinner(20,50)
 
 
-        // Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
+// Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
 
 
-        // const calcAge = function (birthYear) {
-        //     return 2037 - birthYear;
-        // }
+const calcAge = function (birthYear) {
+    return 2037 - birthYear;
+}
 
-        // const yearsUntilRetirement = function (birthYear, firstName) {
-        //     const age = calcAge(birthYear);
-        //     const retirement = 65 - age;
-        //     if (retirement > 0) {
-        //         console.log(`${firstName} retires in ${retirement} years`);
-        //         return retirement;
-        //     } else {
-        //         console.log(`${firstName} has already retired`);
-        //         return -1
-        //     }
+const yearsUntilRetirement = function (birthYear, firstName) {
+    const age = calcAge(birthYear);
+    const retirement = 65 - age;
+    if (retirement > 0) {
+        console.log(`${firstName} retires in ${retirement} years`);
+        return retirement;
+    } else {
+        console.log(`${firstName} has already retired`);
+        return -1
+    }
 
-        //     //return `${firstName} retires in ${retirement} years`;
-        // }
-        // console.log(yearsUntilRetirement(1991, 'Jonas'));
-        // console.log(yearsUntilRetirement(1950, 'Mike'));
-        // console.log(yearsUntilRetirement(1965, 'Jonas'));
+    return `${firstName} retires in ${retirement} years`;
+}
+console.log(yearsUntilRetirement(1991, 'Jonas'));
+console.log(yearsUntilRetirement(1950, 'Mike'));
+console.log(yearsUntilRetirement(1965, 'Jonas'));
