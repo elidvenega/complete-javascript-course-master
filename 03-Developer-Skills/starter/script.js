c34a400c0749a8b5c66810b2a4cb6774b91d9e88
elid venega <venegaelid@gmail.com>
2021-01-25 23:27:09 -0800

    will finish this and become a developer

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -8,3 +8,6 @@
 //there might be a sensor error."
 
 const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+
+// 1} Understanding the problem
+// What is temp amplitude? Answer: di 
