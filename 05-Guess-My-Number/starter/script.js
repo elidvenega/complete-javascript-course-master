c263448222acaf1176cd9774f8f2c596143bc44f
elid venega <venegaelid@gmail.com>
2021-02-01 22:54:48 -0800

    Manage to change bg color

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -13,15 +13,22 @@ document.querySelector('.guess').value = 23;*/
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 
-document.querySelector('.number').textContent = secretNumber;
 document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
     // console.log(guess, typeof guess);
-
+    
+    //  When there is no input
     if (!guess) {
         document.querySelector('.message').textContent = 'No Number';
+    // When player wins
     } else if (guess === secretNumber) {
         document.querySelector('.message').textContent = 'Correct Number🎉';
+        document.querySelector('.number').textContent = secretNumber;
+        document.querySelector('body').style.backgroundColor="#60b347";
+
+        document.querySelector('.number').style.width = "30rem";
+
+     // When guess is too high
     } else if (guess > secretNumber) {
         if (score > 1) {
             document.querySelector('.message').textContent = 'Too High';
@@ -31,7 +38,7 @@ document.querySelector('.check').addEventListener('click', function () {
             document.querySelector('.message').textContent = '💣 You lost the game';
             document.querySelector('.score').textContent = 0;
         }
-
+    // When guess is too low
     } else if (guess < secretNumber) {
         if (score > 1) {
             document.querySelector('.message').textContent = '🏢 Too Low';
@@ -43,6 +50,37 @@ document.querySelector('.check').addEventListener('click', function () {
         }
     }
 });
+
+// Challenge reset guess button and background
+document.querySelector('.again').addEventListener('click', function(){
+    document.querySelector('body').style.backgroundColor = "#222";
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
