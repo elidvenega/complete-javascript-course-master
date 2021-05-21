7abf2848a37145b4343ae1cd1377d6cecff37425
elid venega <venegaelid@gmail.com>
2021-05-21 07:27:22 -0700

    Work on understanding scope in js

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,19 +1,45 @@
-'use strict';
+// 'use strict';
 
-function calcAge(birthYear) {
-    const age = 2037 - birthYear;
-    
-    function printAge() {
-        const output = `${firstName}, you are ${age}, born in ${birthYear}`;
-        console.log(output);
-    }
+// function calcAge(birthYear) {
+//   const age = 2037 - birthYear;
+//   const firstName = "Mike";
 
-  printAge();
+//   function printAge() {
+//     const output = `${firstName}, you are ${age}, 
+//         born in ${birthYear}`;
+//     console.log(output);
 
-    return age;
+//     if (birthYear >= 1981 && birthYear <= 1996) {
+//       let str = `Oh, and you're a millenial, 
+//           ${firstName}`;
+//       console.log(str);
+
+//       function add(a, b) {
+//         return a + b;
+//       }
+//     }
+//     //add(2, 3);
+//   }
+
+//   printAge();
+
+//   return age;
+// }
+
+// const firstName = 'Jonas';
+// calcAge(1991);
+// //console.log(age);
+// // printAge()
+
+console.log(hello);
+
+morning();
+
+var hello = "Have a good day";
+
+console.log(hello);
+console.log(hello);
+
+function morning() {
+  console.log("Hello good morning");
 }
-
-const firstName = 'Jonas';
-calcAge(1991);
-//console.log(age);
-printAge()
