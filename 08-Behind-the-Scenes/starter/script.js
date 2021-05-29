05596340d1ca5bbfc487c4f01c93c495116940b7
elid venega <venegaelid@gmail.com>
2021-05-28 20:19:02 -0700

    finish this keyword lesson

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -51,69 +51,85 @@
 // console.log(year);
 
 
+// Hoisting
 
-var me = 'Jonas';
-var job = 'teacher';
-const year = 1991;
+// var me = 'Jonas';
+// var job = 'teacher';
+// const year = 1991;
 
-// functions 
-console.log(addDecl(2,3));
-//console.log(addExpr(2,3));
-console.log(addArrow);
-//console.log(addArrow(2,3));
+// // functions 
+// console.log(addDecl(2,3));
+// //console.log(addExpr(2,3));
+// console.log(addArrow);
+// //console.log(addArrow(2,3));
 
-function addDecl(a,b) {
-  return a + b;
+// function addDecl(a,b) {
+//   return a + b;
+// }
+
+// const addExpr = function(a,b) {
+//   return a + b;
+// }
+
+// var addArrow = (a,b) => a +b;
+
+// // Example
+// if(!numProducts) deleteShoppingCart();
+
+// var numProducts = 10;
+
+// function deleteShoppingCart() {
+//     console.log('All products deleted');
+// }
+
+// var x = 1;
+// let y = 2;
+// const z = 3;
+
+// console.log(x === window.x);
+// console.log(x === window.y);
+// console.log(x === window.z);
+
+
+// this keyword
+
+// console.log(this);
+
+// const calcAge = function(birthYear) {
+//   console.log(2037 - birthYear);
+//   console.log(this);
+// };
+
+// calcAge(1991);
+
+// const calcAgeArrow = birthYear => {
+//   console.log(2037 - birthYear);
+//   console.log(this);
+// };
+
+// calcAgeArrow(1998);
+
+const jonas = {
+  year: 1991,
+  calcAge: function () {
+    console.log(2037 - this.year);
+
+  }
 }
 
-const addExpr = function(a,b) {
-  return a + b;
-}
-
-var addArrow = (a,b) => a +b;
-
-// Example
-if(!numProducts) deleteShoppingCart();
-
-var numProducts = 10;
-
-function deleteShoppingCart() {
-    console.log('All products deleted');
-}
-
-var x = 1;
-let y = 2;
-const z = 3;
-
-console.log(x === window.x);
-console.log(x === window.y);
-console.log(x === window.z);
+jonas.calcAge();
 
 
+const matilda = {
+  year: 2017,
 
+};
+// method borrowing
+matilda.calcAge = jonas.calcAge;
+matilda.calcAge();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const f = jonas.calcAge;
+f();
 
 
 
