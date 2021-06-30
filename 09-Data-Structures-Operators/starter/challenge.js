fdbfcff1666ba5cef7e78d513213136da612238d
elid venega <venegaelid@gmail.com>
2021-06-30 06:55:06 -0700

    did Udemy challenges

--- 09-Data-Structures-Operators/starter/challenge.js
+++ 09-Data-Structures-Operators/starter/challenge.js
@@ -1,12 +1,15 @@
+'use strict';
 /* 
 We're building a football betting app (soccer for my American friends 😅)!
 
 Suppose we get data from a web service about a certain game (below). In this challenge we're gonna work with the data. So here are your tasks:
 
 1. Create one player array for each team (variables 'players1' and 'players2')
-2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
+2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable 
+('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
 3. Create an array 'allPlayers' containing all players of both teams (22 players)
-4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array 
+('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
 5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
 6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
 7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
@@ -57,5 +60,23 @@ const game = {
     },
 };
 
-const teams = [...game.team1]
-console.log(teams);
+// Challenge 1
+const [players1, players2] = game.players;
+console.log(players1, players2);
+
+// Challenge 2
+const [gk, ...fieldPlayers] = players1;
+console.log(gk, fieldPlayers);
+
+// Challenge 3
+const [allPlayers] = [...players1, ...players2];
+console.log(allPlayers);
+
+// CHallenge 4
+// const [players1Final] = game.players;
+// players1Final.push('Thiago', 'Coutinho', 'Perisic');
+// console.log(players1Final);
+// Jonas answer
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisicic'];
+
+console.log(players1Final);
