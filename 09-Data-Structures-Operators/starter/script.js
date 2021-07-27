ec839ac7becd20bac34465852489196be64dcea0
elid venega <venegaelid@gmail.com>
2021-07-27 07:02:38 -0700

    Started JS Challenge

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -73,8 +73,8 @@ const entries = Object.entries(openingHours);
 //console.log(entries);
 
 // [key,value]
-// Review lesson
-for(const [key, {open,close}] of entries) {
+
+for(constaaaaaaaaaaaaa [key, {open,close}] of entries) {
   console.log(`On ${key} we open at ${open} and close at ${close}`);
 }
 
