f52d26534b84589d715346978ea1bed723964596
elid venega <venegaelid@gmail.com>
2021-11-09 06:43:03 -0800

    Made set work

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -336,7 +336,7 @@ for(const order of orderSet) console.log(order);
 // Example
 const staff = ['waiter','chef','waiter','manager','chef','waiter'];
 const staffUnique = [...new Set(staff)];
-console.log(staffUnique);(
+console.log(staffUnique);
 console.log(
   new Set(['waiter','chef','waiter','manager',
   'chef','waiter']).size
