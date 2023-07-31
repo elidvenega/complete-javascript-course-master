1c513a847e8f69727af687644f16d18c487c29dc
elidvenega <venegaelid@gmail.com>
2023-07-30 23:32:32 -0700

    Updated

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,37 @@
 'use strict';
+
+const lufthansa = {
+  airline: 'Lufthansa',
+  iataCode: 'LH',
+  bookings: [],
+  book(flightNum, name) {
+    console.log(`${name} booked a seat on ${this.airline}
+        flight ${this.iataCode} ${flightNum}`);
+  },
+};
+
+lufthansa.book(12121, 'Jonas Schmedtmann');
+
+const eurowings = {
+  airline: 'Eurowings',
+  iataCode: 'EW',
+  bookings: [],
+};
+
+const book = lufthansa.book;
+
+// Does not work
+// book(23, 'Sarah Williams');
+
+book.call(eurowings, 23, "Sarah Williams")
+
+
+
+
+
+
+
+
+
+
+
