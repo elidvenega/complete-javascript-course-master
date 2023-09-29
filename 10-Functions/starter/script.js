8c64d754367131a11e4ae240d6b582dc33e68659
elidvenega <venegaelid@gmail.com>
2023-09-28 21:34:13 -0700

    Worked with functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,37 +1,18 @@
 'use strict';
+////////////////////////////////////////////////////////////////////////////////////////////////
+// Default Parameters
+const bookings = [];
 
-const lufthansa = {
-  airline: 'Lufthansa',
-  iataCode: 'LH',
-  bookings: [],
-  book(flightNum, name) {
-    console.log(`${name} booked a seat on ${this.airline}
-        flight ${this.iataCode} ${flightNum}`);
-  },
+const createBooking = function (flightNum, numPassengers = 1, price = 199) {
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
 };
 
-lufthansa.book(12121, 'Jonas Schmedtmann');
-
-const eurowings = {
-  airline: 'Eurowings',
-  iataCode: 'EW',
-  bookings: [],
-};
-
-const book = lufthansa.book;
-
-// Does not work
-// book(23, 'Sarah Williams');
-
-book.call(eurowings, 23, "Sarah Williams")
-
-
-
-
-
-
-
-
-
-
-
+createBooking('LH123');
+createBooking('LH234', 3, 455);
+createBooking('LH234', undefined, 255);
