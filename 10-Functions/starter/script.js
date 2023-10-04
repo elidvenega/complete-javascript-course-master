2fdade9ed7831b08e171479cf57b5581e6eb3e7e
elidvenega <venegaelid@gmail.com>
2023-10-03 19:26:46 -0700

    Updated

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -16,3 +16,25 @@ const createBooking = function (flightNum, numPassengers = 1, price = 199) {
 createBooking('LH123');
 createBooking('LH234', 3, 455);
 createBooking('LH234', undefined, 255);
+
+// Pass by value and argument
+const flight = 'LH235';
+const jonas = {
+  name: 'Jonas Schmedtman',
+  passport: 3245422,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr.' + passenger.name;
+
+  if (passenger.passport === 3245422) {
+    alert('Checked In');
+  } else {
+    alert('Wrong Passport');
+  }
+};
+
+console.log(checkIn(flight, jonas));
+console.log(flight);
+console.log(jonas);
